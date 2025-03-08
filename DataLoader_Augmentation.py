923e186c213b916234f58ba89dbed2af3f61e15a
MuhammadAhmad7171 <ahmadjamil7171@gmail.com>
2025-03-09 03:25:10 +0800

    Update DataLoader_Augmentation.py

--- DataLoader_Augmentation.py
+++ DataLoader_Augmentation.py
@@ -1,6 +1,6 @@
 import torch
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, DistributedSampler
 
 def get_train_transform():
     return transforms.Compose([
@@ -20,11 +20,18 @@ def get_test_transform():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-def load_data(train_dir, test_dir, batch_size=32):
+def load_data(train_dir, test_dir, batch_size=32, distributed=False):
     train_dataset = datasets.ImageFolder(train_dir, transform=get_train_transform())
     test_dataset = datasets.ImageFolder(test_dir, transform=get_test_transform())
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    if distributed:
+        train_sampler = DistributedSampler(train_dataset)
+        test_sampler = DistributedSampler(test_dataset, shuffle=False)
+    else:
+        train_sampler = None
+        test_sampler = None
 
-    return train_loader, test_loader
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=(train_sampler is None), num_workers=4, sampler=train_sampler)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, sampler=test_sampler)
+
+    return train_loader, test_loader, train_sampler
