19ebb8405dabf84c865ed5046dda72cdb4e4af22
MuhammadAhmad7171 <ahmadjamil7171@gmail.com>
2025-03-09 03:45:20 +0800

    Update DataLoader_Augmentation.py

--- DataLoader_Augmentation.py
+++ DataLoader_Augmentation.py
@@ -1,6 +1,29 @@
-from torch.utils.data.distributed import DistributedSampler
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, DistributedSampler
+
+def get_train_transform():
+    """Define transformations for the training dataset."""
+    return transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+def get_test_transform():
+    """Define transformations for the test dataset."""
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
 def load_data(train_dir, test_dir, batch_size=32, distributed=False):
+    """Load training and test datasets with optional distributed sampler."""
     # Define transforms
     train_transform = get_train_transform()
     test_transform = get_test_transform()
